9abb80bc1971cd65246e9ebd80c7866b50ee6451
Foremost <lulllaby.fore@gmail.com>
2016-10-07 22:43:41 +0700

    Add tabs

--- app/src/main/java/com/app/fa/restaurantmate_v1/MainActivity.java
+++ app/src/main/java/com/app/fa/restaurantmate_v1/MainActivity.java
@@ -1,19 +1,45 @@
 package com.app.fa.restaurantmate_v1;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.app.fa.restaurantmate_v1.Activity.OrderActivity;
+import com.app.fa.restaurantmate_v1.fragment.MenuTabFragment;
+import com.app.fa.restaurantmate_v1.fragment.OrderFoodNowTabFragment;
+import com.app.fa.restaurantmate_v1.fragment.OrderFoodTotalTabFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+//    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        databaseReference.child("most").setValue("Foremost");
+        // order page
+        Button orderButton = (Button)findViewById(R.id.order_button);
+        orderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
