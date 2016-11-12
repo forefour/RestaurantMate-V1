fc8936446b96acd5798f31b71c487edb0ee95d4e
Foremost <fourforemost@gmail.com>
2016-11-12 13:27:47 +0700

    admin + table

--- app/src/main/java/com/app/fa/restaurantmate_v1/fragment/admin/TableFragment.java
+++ app/src/main/java/com/app/fa/restaurantmate_v1/fragment/admin/TableFragment.java
@@ -1,19 +1,27 @@
 package com.app.fa.restaurantmate_v1.fragment.admin;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.app.fa.restaurantmate_v1.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TableFragment extends Fragment {
-
+    private FloatingActionButton fab;
 
     public TableFragment() {
         // Required empty public constructor
@@ -25,6 +33,40 @@ public class TableFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_table, container, false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        fab = (FloatingActionButton) getView().findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                View viewRoot = null;
+                viewRoot = getActivity().getLayoutInflater().inflate(R.layout.admin_table_dialog, null);
+                Log.d("FloatingActionButton","FloatingActionButton");
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getContext(),R.style.YourDialogStyle);
+                builder.setTitle("แก้ไขจำนวนโต๊ะอาหาร");
+                builder.setView(viewRoot);
+                builder.setPositiveButton("แก้ไข", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("foodNowDialog","OK");
+                        Toast toast = Toast.makeText(getContext(),"แก้ไขจำนวนโต๊ะเรียบร้อยแล้ว", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
+                builder.setNegativeButton("ยกเลิก", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("foodNowDialog","Cancel");
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 
 }
