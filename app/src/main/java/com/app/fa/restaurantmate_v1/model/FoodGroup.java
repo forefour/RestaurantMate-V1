08aa478b434814c8bc8da363dcac75e0b577acf6
Foremost <fourforemost@gmail.com>
2016-11-22 11:14:38 +0700

    admin backend 1

---
+++ app/src/main/java/com/app/fa/restaurantmate_v1/model/FoodGroup.java
@@ -0,0 +1,27 @@
+package com.app.fa.restaurantmate_v1.model;
+
+/**
+ * Created by Foremost on 22/11/2559.
+ */
+
+public class FoodGroup {
+
+    String name;
+
+    public FoodGroup() {
+    }
+
+    public FoodGroup(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
